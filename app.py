402488e5ed837cc9d0fbb2a33264b5ccd79cb55c
corentin-glanum <88728414+corentin-glanum@users.noreply.github.com>
2021-11-29 16:34:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ app_mode = st.sidebar.selectbox("Choose the app mode",
     ["Training", "Testing"])
 if app_mode == "Training":
     img_file_buffer = st.file_uploader("Choose the training set")
+    model_picker = st.radio("Quel modèle utiliser?", ('AlexNet', 'AlexNet aussi'))
     st.write('yo')
 elif app_mode == "Testing":
     #=================================== Title ===============================
